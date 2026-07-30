b23c013410c3b59398b8f01033d364277c8edc5d
gabj1-gildo <gabj1@aluno.ifnmg.edu.br>
2026-07-30 16:56:25 -0300

    chore: remove root docs and js scripts from tracking

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const f = 'front-end/src/app/(landing)/planos/page.tsx';
-let content = fs.readFileSync(f, 'utf8');
-content = content.split('\\`').join('`');
-fs.writeFileSync(f, content);
-console.log('Fixed ' + f);
