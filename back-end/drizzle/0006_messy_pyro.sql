27e37d05487c82dfb5d64df9d542a2657d59ce93
gabj1-gildo <gabj1@aluno.ifnmg.edu.br>
2026-09-23 13:05:42 -0300

    deploy website

--- back-end/drizzle/0006_messy_pyro.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "ai_models" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"provider" varchar(50) NOT NULL,
-	"model_id" varchar(100) NOT NULL,
-	"name" varchar(150) NOT NULL,
-	"is_active" boolean DEFAULT true NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
--- back-end/drizzle/0021_dashing_frank_castle.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "tenant_plans" ADD COLUMN "max_installments" integer DEFAULT 1;--> statement-breakpoint
-ALTER TABLE "tenant_plans" ADD COLUMN "interest_absorption" varchar(20) DEFAULT 'BUYER';
